f5f8ce1d6676fc8dd3a1036b9c4fa726511ce487
C0nnwaer <webellachung@gmail.com>
2020-02-06 09:05:43 -0500

    Create button and write onClick function in main activity

--- app/src/main/java/com/chungbella/buttonapp/MainActivity.java
+++ app/src/main/java/com/chungbella/buttonapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.chungbella.buttonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit_button = findViewById(R.id.test_button);
+        submit_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.i("testButton", "Hey what's up? :)");
+            }
+        });
     }
 }
